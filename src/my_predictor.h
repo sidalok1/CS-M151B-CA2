8c5763f02977aad50edb389aa0c5dc4440266ad5
Sidharth Alok Girdhar <116860545+sidalok1@users.noreply.github.com>
2024-11-17 00:47:59 -0800

    MPKI: 4.096
    Final Submission

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <ostream>
 #include <cmath>
+#include <bitset>
 using namespace std;
 class my_update : public branch_update {
 public:
@@ -18,23 +19,24 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	32
-#define TABLE_BITS	15
-#define BTB_BITS 8
-#define PERCEPTRON_BITS 16
+#define HISTORY_LENGTH	75
+#define GHISTORY_LENGTH	14
+#define TABLE_BITS	18
+#define PERCEPTRON_BITS 18
 #define PERCEPTRON_MASK ((1 << PERCEPTRON_BITS) - 1)
 	my_update u;
 	branch_info bi;
-	unsigned int history; //Global hist
+	unsigned long long history; //Global hist
+	unsigned int ghistory; //For gshare
+	bitset<HISTORY_LENGTH> histBits;
 	unsigned char tab[1<<TABLE_BITS]; //Local hist
-	unsigned int btb[1<<BTB_BITS][2]; //branch target map
-	int pweights[1<<PERCEPTRON_BITS][HISTORY_LENGTH];
+	unsigned char choice = 0; //Decision mechanism
+	int pweights[1<<PERCEPTRON_BITS][HISTORY_LENGTH]; //ANN weights
+	int gshare_weight = 0;
+	const unsigned int thresh = floor((1.93 * (HISTORY_LENGTH + 1)) + 14) ; // https://www.cs.utexas.edu/~lin/papers/hpca01.pdf
 
-	const unsigned int thresh = floor(1.93 * HISTORY_LENGTH) + 14; // https://www.cs.utexas.edu/~lin/papers/hpca01.pdf
-
-	my_predictor (void) : history(0) { 
+	my_predictor (void) : history(0), ghistory(0) {
 		memset (tab, 0, sizeof (tab));
-		memset (btb, 0, sizeof (btb));
 		memset (pweights, 0, sizeof (pweights));
 	}
 
@@ -43,33 +45,28 @@ public:
 		if (b.br_flags & BR_CONDITIONAL) {
 			//Gshare table index
 			u.index = //Hash
-				  (history << (TABLE_BITS - HISTORY_LENGTH))
+				  (ghistory << (TABLE_BITS - GHISTORY_LENGTH))
 				^ (b.address & ((1<<TABLE_BITS)-1));
 
 			u.w_index =
 					(b.address & PERCEPTRON_MASK)
 					^ ((b.address << PERCEPTRON_BITS) & PERCEPTRON_MASK);
-			u.sum = 0;
-			for (auto i = 0; i < HISTORY_LENGTH; i++) {
-				unsigned int hist_bit = (history >> i) & 1;
-				if (hist_bit == 1) {
-					u.sum += pweights[u.w_index][i];
-				} else {
-					u.sum -= pweights[u.w_index][i];
-				}
-			}
 
-			u.direction_prediction (u.sum >= 0);
+
+
+			u.sum = tab[u.index]>>2 ? gshare_weight : -gshare_weight;
+
+			histBits = bitset<HISTORY_LENGTH>(history);
+
+			for (auto i = 0; i < HISTORY_LENGTH; i++) {
+				u.sum += histBits[i] ? pweights[u.w_index][i] : - pweights[u.w_index][i];
+			}
+			const bool decision = (choice>>1) ? (u.sum > 0) : tab[u.index]>>2;
+			u.direction_prediction (decision);
 		} else { //Unconditional jump
 			u.direction_prediction (true);
 		}
-		if (btb[b.address & ((1<<BTB_BITS)-1)][0] == (b.address & ~((1<<BTB_BITS)-1))) {
-			u.target_hit = true;
-			u.target_prediction (btb[b.address & ((1<<BTB_BITS)-1)][1]);
-			//cerr << "HIT: " << u.target_prediction() << endl;
-		} else {
-			u.target_hit = false;
-		}
+		u.target_prediction (0);
 		return &u;
 	}
 
@@ -77,9 +74,24 @@ public:
 		my_update* mu = (my_update*)u;
 		if (bi.br_flags & BR_CONDITIONAL) {
 
-			if (taken != mu->direction_prediction() or abs(mu->sum) < thresh) {
+			if (mu->sum > 0 == taken) {
+				//Update choice mechanism. There is a bias towards the perceptron
+				if (choice < 3) {
+					choice++;
+				} else if (choice > 0) {
+					choice--;
+				}
+			}
+
+			if (taken != mu->direction_prediction() or abs(mu->sum) <= thresh) {
+				//Update the weights for the ANN
+				if (tab[mu->index]>>2 == taken) {
+					gshare_weight++;
+				} else {
+					gshare_weight--;
+				}
 				for (auto i = 0; i < HISTORY_LENGTH; i++) {
-					if (((history >> i) & 1) == taken) {
+					if (histBits[i] == taken) {
 						pweights[mu->w_index][i]++;
 					}
 					else {
@@ -92,17 +104,17 @@ public:
 
 			unsigned char *c = &tab[mu->index]; //Pointer to local history
 			if (taken) {
-				if (*c < 3) (*c)++;
+				if (*c < 7) (*c)++;
 			} else {
 				if (*c > 0) (*c)--;
 			}
+
 			history <<= 1; //Global history
 			history |= taken;
 			history &= (1<<HISTORY_LENGTH)-1;
-		}
-		if (mu->target_hit == false or btb[bi.address & ((1<<BTB_BITS)-1)][1] != target) {
-			btb[bi.address & ((1<<BTB_BITS)-1)][0] = bi.address & ~((1<<BTB_BITS)-1);
-			btb[bi.address & ((1<<BTB_BITS)-1)][1] = target;
+			ghistory <<= 1; //Global history for gshare
+			ghistory |= taken;
+			ghistory &= (1<<GHISTORY_LENGTH)-1;
 		}
 	}
 };
